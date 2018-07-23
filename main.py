01663addae348388f73a79f5a08a6fd12aaceb34
Keirten Nivol <keirtennivol@gmail.com>
2018-07-23 15:26:52 -0400

    main

---
+++ main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import webapp2
+import os
+import jinja2
+#from models import
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+#remember, you can get this by searching for jinja2 google app engine
+jinja_current_dir = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Welcome to travelbook")
+
+
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+
+class TripsHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+
+class TimelineHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+class MapHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/login', LoginHandler),
+    #('/trips/../upload', )
+    ('/trips', TripsHandler),
+    ('/map', MapHandler),
+    ('/trips/..', TimelineHandler),
+], debug=True)
