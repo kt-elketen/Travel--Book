be754c6363673816c1698a2a11021e10f7c7a7ec
Keirten Nivol <keirtennivol@gmail.com>
2018-07-26 11:54:12 -0400

    Timeline

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import os
 import jinja2
 import login
 import maps
-
-
+from google.appengine.api import images
 #remember, you can get this by searching for jinja2 google app engine
 #jinja_current_dir = jinja2.Environment(
 #    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -32,10 +31,15 @@ class TripListHandler(webapp2.RequestHandler):
         pass
 class TripTimelineHandler(webapp2.RequestHandler):
     def get(self):
+        timeline_data = {
+        'picture_date': Pictures.date,
+        'picture_place': Pictures.location,
+        'picture_description': Pictures.description,
+        'trip_name': Trip.name
+        }
         form_template = jinja_env.get_template('templates/triptimeline.html')
-        html = form_template.render()
+        html = form_template.render(timeline_data)
         self.response.write(html)
-        pass
 
 
 class UploadHandler(webapp2.RequestHandler):
@@ -44,33 +48,32 @@ class UploadHandler(webapp2.RequestHandler):
         html = form_template.render()
         self.response.write(html)
     def post(self):
-        data = Data()
-        data.trip = self.request.get('trip')
-        data.location = self.request.get('location')
-        data.date = self.request.get('date')
-        data.description = self.request.get('description')
-        data.comments = self.request.get('comments')
-        data.information = self.request.get('information')
-        data.image = image.resize(self.request.get('image'), 250, 250)
-        data.put()
+        Trip.name = self.request.get('trip')
+        Pictures.location = self.request.get('location')
+        Pictures.date = self.request.get('date')
+        Pictures.description = self.request.get('description')
+        Pictures.comments = self.request.get('comments')
+        Pictures.information = self.request.get('information')
+        Pictures.image = images.resize(self.request.get('image'), 250, 250)
+        Pictures.put()
 
 # Model for an image:
 from google.appengine.ext import ndb
-class Data(ndb.Model):
-    trip = ndb.StringProperty(required = True)
+class Pictures(ndb.Model):
     location = ndb.StringProperty(required = True)
     date = ndb.StringProperty(required = True)
     description = ndb.StringProperty(required = False)
     comments = ndb.StringProperty(required = False)
-    information = ndb.StringProperty(required = False)
     image = ndb.BlobProperty(required = True)
-
+class Trip(ndb.Model):
+    name = ndb.StringProperty(required= True)
+    pictures = ndb.KeyProperty(Pictures, repeated= True)
 
 
 
 class Image(webapp2.RequestHandler):
     def get(self):
-        key = ndb.Key("Data", int(self.request.get("id")))
+        key = ndb.Key("Pictures", int(self.request.get("id")))
         data = key.get()
         self.response.headers['Content-Type'] = 'image/jpg'
         self.response.write(data.image)
@@ -82,7 +85,6 @@ app = webapp2.WSGIApplication([
       ('/', MainHandler),
       ('/login', login.LoginHandler),
       ('/trips', TripsHandler),
-
       ('/triplist', TripListHandler),
       ('/maps', maps.MapsHandler),
       ('/maps/record_request', maps.RecordRequestHandler),
