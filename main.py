d41c7eae1840c972d120b40a5132a29f5c968cd5
Waziha Wadud <wazihawadud@gmail.com>
2018-07-26 11:53:08 -0400

    worked on 2 classes

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ class TripListHandler(webapp2.RequestHandler):
         form_template = jinja_env.get_template('templates/triplist.html')
         html = form_template.render()
         self.response.write(html)
-    Data.query().fetch()
 
 class TripTimelineHandler(webapp2.RequestHandler):
     def get(self):
@@ -41,6 +40,16 @@ class TripTimelineHandler(webapp2.RequestHandler):
         self.response.write(html)
         pass
 
+# Model for an image:
+class Picture(ndb.Model):
+    location = ndb.StringProperty(required = True)
+    date = ndb.StringProperty(required = True)
+    description = ndb.StringProperty(required = False)
+    image = ndb.BlobProperty(required = True)
+
+class Trip(ndb.Model):
+    name = ndb.StringProperty(required = True)
+    pictures = ndb.KeyProperty(Picture, repeated = True)
 
 class UploadHandler(webapp2.RequestHandler):
     def get(self):
@@ -48,36 +57,35 @@ class UploadHandler(webapp2.RequestHandler):
         html = form_template.render()
         self.response.write(html)
     def post(self):
-        data = Data()
-        data.trip = self.request.get('trip')
-        data.location = self.request.get('location')
-        data.date = self.request.get('date')
-        data.description = self.request.get('description')
-        data.comments = self.request.get('comments')
-        data.information = self.request.get('information')
-        data.image = images.resize(self.request.get('image'), 250, 250)
-        data.put()
+        picture = Picture()
+        picture.location = self.request.get('location')
+        picture.date = self.request.get('date')
+        picture.description = self.request.get('description')
+        picture.image = images.resize(self.request.get('image'), 250, 250)
+        picture_key =picture.put()
+        trip = Trip()
+        trip.name=self.request.get('trip')
+        trip.pictures=[picture_key]
+        trip.put()
         self.redirect("/triplist")
-
-# Model for an image:
-class Data(ndb.Model):
-    trip = ndb.StringProperty(required = True)
-    location = ndb.StringProperty(required = True)
-    date = ndb.StringProperty(required = True)
-    description = ndb.StringProperty(required = False)
-    comments = ndb.StringProperty(required = False)
-    information = ndb.StringProperty(required = False)
-    image = ndb.BlobProperty(required = True)
-
-
-
 
 class Image(webapp2.RequestHandler):
     def get(self):
-        key = ndb.Key("Data", int(self.request.get("id")))
+        #key = ndb.Key("Data", int(self.request.get("id")))
+        #data = key.get()
+        #self.response.headers['Content-Type'] = 'image/jpg'
+        #self.response.write(data.image)
+        key = ndb.Key(urlsafe=self.request.get('img_id'))
         data = key.get()
-        self.response.headers['Content-Type'] = 'image/jpg'
-        self.response.write(data.image)
+        if data.image:
+            self.response.headers['Content-Type'] = 'image/png'
+            self.response.out.write(data.image)
+        else:
+            self.response.out.write('No image')
+        self.response.out.write('<div><img src="/img?img_id=%s"></img>' %
+                        data.key.urlsafe())
+        #self.response.out.write('<blockquote>%s</blockquote></div>' %
+        #                cgi.escape(data.content))
 
 
 
